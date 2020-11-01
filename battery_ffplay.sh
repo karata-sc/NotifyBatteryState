e958c7f3175c058f586f72766652aaeee081de49
karata-sc <52286662+karata-sc@users.noreply.github.com>
2020-11-01 22:37:35 +0900

    Create battery_ffplay.sh

---
+++ battery_ffplay.sh
@@ -0,0 +1,17 @@
+#!/bin/sh                                                                                        
+DISPLAY=:0
+BAT_PER=`/bin/cat "/sys/class/power_supply/BAT0/capacity"`
+BAT_STATE=`/bin/cat "/sys/class/power_supply/BAT0/status"`
+
+if [ $BAT_PER -le 25 -a "$BAT_STATE" = "Discharging" ]
+then
+¦       ffplay /home/orange/Music/battery_notify.wav
+fi
+
+if [ $BAT_PER -ge 80 -a "$BAT_STATE" = "Charging" ]
+then
+¦       ffplay /home/orange/Music/battery_notify.wav
+fi
+
+#sudo apt install ffmpeg
+#*/5 *¦  * * *¦  orange¦ DISPLAY=:0 /home/orange/bin/battery_ffplay.sh
