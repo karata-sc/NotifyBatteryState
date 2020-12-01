cb88e9a1259533ad843ba1ebb867e802b0b55ba9
karata-sc <52286662+karata-sc@users.noreply.github.com>
2020-12-02 03:06:21 +0900

    Update battery_ffplay.sh

--- battery_ffplay.sh
+++ battery_ffplay.sh
@@ -5,13 +5,16 @@ BAT_STATE=`/bin/cat "/sys/class/power_supply/BAT0/status"`
 
 if [ $BAT_PER -le 25 -a "$BAT_STATE" = "Discharging" ]
 then
-¦       ffplay /home/orange/Music/battery_notify.wav
+        ffplay /home/foo/Music/battery_notify.wav
 fi
 
 if [ $BAT_PER -ge 80 -a "$BAT_STATE" = "Charging" ]
 then
-¦       ffplay /home/orange/Music/battery_notify.wav
+        ffplay /home/foo/Music/battery_notify.wav
 fi
 
 #sudo apt install ffmpeg
-#*/5 *¦  * * *¦  me¦ DISPLAY=:0 /home/me/bin/battery_ffplay.sh
+
+#cron setting
+#
+#*/5 *¦  * * *¦  me¦ DISPLAY=:0 /home/foo/bin/battery_ffplay.sh
